22b782ea079cf06848b845704d89063a4838597c
Patrick Walters <wpwalters@gmail.com>
2022-11-20 20:04:54 -0500

    Code refactoring

--- ring_systems.py
+++ ring_systems.py
@@ -8,14 +8,13 @@ from tqdm.auto import tqdm
 
 class RingSystemFinder:
     def __init__(self):
-        """
-        Initialize susbstructure search objects to identify key functionality
+        """Initialize susbstructure search objects to identify key functionality
         """
         self.ring_db_pat = Chem.MolFromSmarts("[#6R,#18R]=[OR0,SR0,CR0,NR0]")
         self.ring_atom_pat = Chem.MolFromSmarts("[R]")
 
     def tag_bonds_to_preserve(self, mol):
-        """ Assign the property "protected" to all ring carbonyls, etc.
+        """Assign the property "protected" to all ring carbonyls, etc.
         :param mol: input molecule
         :return: None
         """
@@ -28,7 +27,7 @@ class RingSystemFinder:
 
     @staticmethod
     def cleave_linker_bonds(mol):
-        """ Cleave bonds that are not in rings and not protected
+        """Cleave bonds that are not in rings and not protected
         :param mol: input molecule
         :return: None
         """
@@ -40,19 +39,19 @@ class RingSystemFinder:
         if len(frag_bond_list):
             frag_mol = Chem.FragmentOnBonds(mol, frag_bond_list)
             Chem.SanitizeMol(frag_mol)
+            # Chem.AssignStereochemistry(frag_mol, cleanIt=True, force=True)
             return frag_mol
         else:
             return mol
 
     def cleanup_fragments(self, mol, keep_dummy=False):
-        """
-        split a molecule containing multiple ring systems into individual ring systems
+        """Split a molecule containing multiple ring systems into individual ring systems
         :param keep_dummy: retain dummy atoms
         :param mol: input molecule
         :return: a list of SMILES corresponding to individual ring systems
         """
         frag_list = Chem.GetMolFrags(mol, asMols=True)
-        ring_system_smiles_list = []
+        ring_system_list = []
         for frag in frag_list:
             if frag.HasSubstructMatch(self.ring_atom_pat):
                 for atm in frag.GetAtoms():
@@ -64,31 +63,51 @@ class RingSystemFinder:
                         atm.SetIsotope(0)
                 # Convert explict Hs to implicit
                 frag = Chem.RemoveAllHs(frag)
-                ring_system_smiles_list.append(Chem.MolToSmiles(frag))
-        return ring_system_smiles_list
+                frag = self.fix_bond_stereo(frag)
+                ring_system_list.append(frag)
+        return ring_system_list
 
-    def find_ring_systems(self, mol, keep_dummy=False):
+    @staticmethod
+    def fix_bond_stereo(mol):
+        """Loop over double bonds and change stereo specification for double bonds that don't have stereo
+        :param mol: input RDKit molecule
+        :return: output RDKit molecule
         """
-        find the ring systems for an RDKit molecule
+        for bnd in mol.GetBonds():
+            if bnd.GetBondType() == Chem.BondType.DOUBLE:
+                begin_atm = bnd.GetBeginAtom()
+                end_atm = bnd.GetEndAtom()
+                # Look for double bond atoms with two attached hydrogens
+                if begin_atm.GetDegree() == 1 or end_atm.GetDegree() == 1:
+                    bnd.SetStereo(Chem.BondStereo.STEREONONE)
+        return mol
+
+    def find_ring_systems(self, mol, keep_dummy=False, as_mols=False):
+        """Find the ring systems for an RDKit molecule
+        :param as_mols: return results a molecules (otherwise return SMILES)
         :param keep_dummy: retain dummy atoms
         :param mol: input molecule
         :return: a list of SMILES corresponding to individual ring systems
         """
         self.tag_bonds_to_preserve(mol)
         frag_mol = self.cleave_linker_bonds(mol)
-        ring_system_smiles_list = self.cleanup_fragments(frag_mol, keep_dummy=keep_dummy)
-        return ring_system_smiles_list
+        output_list = self.cleanup_fragments(frag_mol, keep_dummy=keep_dummy)
+        if not as_mols:
+            output_list = [Chem.MolToSmiles(x) for x in output_list]
+        return output_list
 
 
 def test_ring_system_finder():
+    """A quick test for the RingSystemFinder class
+    :return: None
+    """
     mol = Chem.MolFromSmiles("CC(=O)[O-].CCn1c(=O)/c(=C2\Sc3ccccc3N2C)s/c1=C\C1CCC[n+]2c1sc1ccccc12")
     ring_system_finder = RingSystemFinder()
     ring_system_finder.find_ring_systems(mol)
 
 
 def create_ring_dictionary(input_smiles, output_csv):
-    """
-    read a SMILES file, extract ring systems, write out ring systems and frequency
+    """Read a SMILES file, extract ring systems, write out ring systems and frequency
     :param input_smiles: input SMILES file
     :param output_csv: output csv file
     :return: None
